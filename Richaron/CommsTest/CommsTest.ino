508bb8c8dd8b94b43cf11e4c40020e933fdaf6a8
Forrest Milner <fmilner@zoom456.com>
2022-03-09 15:14:08 -0500

    Added some code and the helpers.h library to libraries.

---
+++ Richaron/CommsTest/CommsTest.ino
@@ -0,0 +1,11 @@
+#include <helpers.h>
+
+void setup() {
+    Serial.begin(115200);
+    //psl stands for print static, with new line. dw about this for now, just know it prints 
+    psl("Richaron has started and is working. Yay!");
+}
+
+void loop() {
+    
+}
---
+++ libraries/helpers/helpers.h
@@ -0,0 +1,30 @@
+//So adding an l will add a println
+#define put(x) {Serial.print(F(#x ": ")); Serial.print(x);}
+#define putn(x, n) {Serial.print(F(#x ": ")); Serial.print(x, n);}
+#define putl(x) {Serial.print(F(#x ": ")); Serial.println(x);}
+#define putnl(x, n) {Serial.print(F(#x ": ")); Serial.println(x, n);}
+#define ps(x) Serial.print(F(x));
+#define psl(x) Serial.println(F(x));
+#define p(x) Serial.print(x);
+#define pl(x) Serial.println(x);
+
+#define plot(x) {Serial.print(F(#x ":")); Serial.print(x); Serial.print('\t');}
+
+#define putArrLong(arr, n) { for(int i = 0; i < n; i++) {ps(#arr "["); p(i); ps("] = "); pl(arr[i]);} };
+#define putArrLongl(arr, n) { for(int i = 0; i < n; i++) {ps(#arr "["); p(i); ps("] = "); pl(arr[i]);} };
+
+#define putArr(arr, n) { ps(#arr "[] =  {"); for(int i = 0; i < n; i++) {p(arr[i]); if(i!=n-1) ps(", ");} ps("};")};
+#define putArrl(arr, n) { ps(#arr "[] =  {"); for(int i = 0; i < n; i++) {p(arr[i]); if(i!=n-1) ps(", ");} psl("};")};
+
+
+#define prgb(c) {Serial.print("{"); Serial.print(c.r); Serial.print(","); Serial.print(c.g); Serial.print(","); Serial.print(c.b); Serial.print("}");};
+#define prgbl(c) {Serial.print("{"); Serial.print(c.r); Serial.print(","); Serial.print(c.g); Serial.print(","); Serial.print(c.b); Serial.println("}");};
+#define waitForKey(during) {while (!Serial.available()) {during}; while (Serial.available()) Serial.read();}
+#define every(n, during) {static int INTERNAL_COUNTER = 0; INTERNAL_COUNTER++; if(INTERNAL_COUNTER == n) {{during}; INTERNAL_COUNTER=0;}};
+#define exit() {while(true);}
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void(* reset) (void) = 0;
